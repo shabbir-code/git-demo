573cb9f781300dae4fd8553c6efd7daa0330c1ff
unknown <techie.shabbir@gmail.com>
2020-09-08 22:50:44 +0530

    changed welcome message

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello javaTpoint";
+		return "Hello Shabbir";
 	}
 }
