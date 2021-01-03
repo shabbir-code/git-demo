8772b92453cd745e517f8aaac1212d6c389a4225
unknown <techie.shabbir@gmail.com>
2021-01-03 15:10:03 +0530

    modiied test cases

--- src/test/java/com/javatpoint/springboothelloworldexample/SpringBootHelloWorldExampleApplicationTests.java
+++ src/test/java/com/javatpoint/springboothelloworldexample/SpringBootHelloWorldExampleApplicationTests.java
@@ -1,13 +1,23 @@
 package com.javatpoint.springboothelloworldexample;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+//import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class SpringBootHelloWorldExampleApplicationTests {
+	/*
+	public static Logger logger = LoggerFactory.getLogger(SpringBootHelloWorldExampleApplicationTests.class);
 
-	@Test
+	//@Test
 	void contextLoads() {
-	}
+		assertEquals(true,true);
+		
+		
+	}*/
+
+	
 
 }
