b288635037ba12483e41cae109148571807ba198
shabbir-code <70969057+shabbir-code@users.noreply.github.com>
2021-06-21 22:19:19 +0530

    Updated HelloWorldController.java

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -13,40 +13,5 @@ public class HelloWorldController {
 		return "Hello UNITED STATES OF AMERICA";
 		
 	}
-
-	public void deleteMethod() {
-
-		System.out.println("I am delete Method");
-		System.out.println("I am delete again");
-
-	}
-
-	public String addMethod() {
-
-		return "I am from add Method..........";
-
-	}
 	
-	public String calculateTripDeails(){
-		
-		
-		return "I will calculate TRIP details ";
-	}
-	
-	public String getAddress(){
-		
-		return "I will bring address";
-	}
-	
-
-	public String getReservation(){
-		
-		return "reservation demo";
-	}
-
-	public String getCreditCardDetails(){
-		
-		return "credit card detaitls";
-	}	
-
 }
